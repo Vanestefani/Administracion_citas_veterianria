4995220aa91ef587bd14500432311f1a54388a17
vanessa <vanessa.estefania.corredor@gmail.com>
2020-04-11 19:25:18 -0500

    reogrganizando proyecto

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Fragment, useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Formulario from "./componentes/Formulario";
+import Header from "./componentes/Header";
+import Citas from "./componentes/Citas";
+
+function App() {
+  //arreglo citas
+  const [citas, guardarCitas] = useState([]);
+  //Agregar citas
+  const crearCitas = (cita) => {
+    guardarCitas([...citas, cita]);
+  };
+  //Eliminar citas
+  // Función que elimina una cita por su id
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter((cita) => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  };
+  // Mensaje condicional
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus Citas";
+  //citas local strorage
+  let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+  useEffect(() => {
+    let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+
+    if (citasIniciales) {
+      localStorage.setItem("citas", JSON.stringify(citas));
+    } else {
+      localStorage.setItem("citas", JSON.stringify([]));
+    }
+  }, [citas]);
+
+  return (
+    <Fragment>
+      <div className="App">
+        <Header></Header>
+        <div className="row">
+          <div class="col-md-6">
+            <Formulario crearCitas={crearCitas}></Formulario>
+          </div>
+          <div class="col-md-6">
+            <h2>{titulo}</h2>
+            {citas.map((cita) => (
+              <Citas
+                key={cita.id}
+                cita={cita}
+                eliminarCita={eliminarCita}
+              ></Citas>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/componentes/Header.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <nav className="navbar navbar-dark bg-primary">
+
+        <h1 className="text-center text-light">
+         🐶Citas veterinarias
+        </h1>
+      </nav>
+    </Fragment>
+  );
+};
+
+export default Header;
